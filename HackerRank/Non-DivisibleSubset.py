b7cd81b3de3a9038ae7aa8e8f88ce9132dbfbc41
Harsh Bajpai <2k23.aids2311075@gmail.com>
2025-09-06 14:06:54 +0530

    Update Non-DivisibleSubset.py

--- HackerRank/Non-DivisibleSubset.py
+++ HackerRank/Non-DivisibleSubset.py
@@ -1,1 +1,17 @@
+def nonDivisibleSubset(k, s):
+    freq = [0] * k
+    
+    for num in s:
+        freq[num % k] += 1
+    result = 0
+    if freq[0] > 0:
+        result += 1
+    for i in range(1, (k // 2) + 1):
+        if i == k - i:  
+            if freq[i] > 0:
+                result += 1
+        else:
+            result += max(freq[i], freq[k - i])
+    
+    return result
 
