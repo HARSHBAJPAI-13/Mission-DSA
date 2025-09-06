289b5562c407d4db097ec2fc7b46ea65324d8fb9
Harsh Bajpai <2k23.aids2311075@gmail.com>
2025-09-06 13:12:24 +0530

    Update 1700-NumberofStudentsUnabletoEatLunch.py

--- Leetcode/1700-NumberofStudentsUnabletoEatLunch.py
+++ Leetcode/1700-NumberofStudentsUnabletoEatLunch.py
@@ -1,14 +1,16 @@
 from collections import deque
-def countStudents(students, sandwiches):
-    students_queue = deque(students)
-    sandwiches_queue = deque(sandwiches)
-    rotations = 0  
-    while students_queue and sandwiches_queue and rotations < len(students_queue):
-        if students_queue[0] == sandwiches_queue[0]:
-            students_queue.popleft()
-            sandwiches_queue.popleft()
-            rotations = 0  
-        else:
-            students_queue.append(students_queue.popleft())
-            rotations += 1
-    return len(students_queue)
+
+class Solution:
+    def countStudents(self, students, sandwiches):
+        students_queue = deque(students)
+        sandwiches_queue = deque(sandwiches)
+        rotations = 0  
+        while students_queue and sandwiches_queue and rotations < len(students_queue):
+            if students_queue[0] == sandwiches_queue[0]:
+                students_queue.popleft()
+                sandwiches_queue.popleft()
+                rotations = 0  
+            else:
+                students_queue.append(students_queue.popleft())
+                rotations += 1
+        return len(students_queue)
